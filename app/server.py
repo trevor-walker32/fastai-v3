5ddb824d886d4398ae5d5aa89682833350ca4d24
Trevor Walker <35089088+trevor-walker32@users.noreply.github.com>
2019-02-07 15:31:22 -0800

    Update server.py

--- app/server.py
+++ app/server.py
@@ -8,7 +8,7 @@ from io import BytesIO
 from fastai import *
 from fastai.vision import *
 
-export_file_url = 'https://www.dropbox.com/s/ovhwg6cflksbett/export.pkl?dl=0'
+export_file_url = 'https://drive.google.com/uc?export=download&id=1U2_SxhOqH8HqH7UYYVtT4s3MAKvVMHj6'
 export_file_name = 'export.pkl'
 
 classes = ['Corgi', 'Husky', 'Shepherd']
